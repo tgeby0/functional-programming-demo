c0fd2d116c7e88fb175e9330a6fa172c64b897b5
mcsaunders02 <milesmanmusic@yahoo.com>
2024-10-28 12:11:58 -0400

    Data types

--- src/main.hs
+++ src/main.hs
@@ -1,13 +1,8 @@
-sumPowers :: Int -> [Int] -> Int
-sumPowers _ [] = 0
-sumPowers power (x : xs) = (x ^ power) + sumPowers power xs
 
-sumPowers2 = sumPowers 2
-sumPowers3 = sumPowers 3
 
-sum1 = sumPowers2 [1, 2, 3, 4] -- 30
-sum2 = sumPowers3 [1, 2, 3, 4] -- 100
+-- Calls a function on a Maybe value if it exists, returning the result, or returns a default value otherwise
+callOrDefault :: (a -> b) -> b -> Maybe a -> b
+callOrDefault fn _ (Just x) = fn x
+callOrDefault _ defaultValue Nothing = defaultValue
 
-main = do
-    print sum1
-    print sum2
+main = print (callOrDefault (*2) 5 (Nothing))
