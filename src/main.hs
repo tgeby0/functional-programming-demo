11b3362a388e1ec443109cd6c8a20c05ae53516f
mcsaunders02 <milesmanmusic@yahoo.com>
2024-10-28 12:12:40 -0400

    Removed stuff in main.hs

--- src/main.hs
+++
@@ -1,8 +0,0 @@
-
-
--- Calls a function on a Maybe value if it exists, returning the result, or returns a default value otherwise
-callOrDefault :: (a -> b) -> b -> Maybe a -> b
-callOrDefault fn _ (Just x) = fn x
-callOrDefault _ defaultValue Nothing = defaultValue
-
-main = print (callOrDefault (*2) 5 (Nothing))
